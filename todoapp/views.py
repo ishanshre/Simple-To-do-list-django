6b5693e5134351d2b59866774708790fa261e90f
Ishan Shrestha <ishan.shrestha356@gmail.com>
2022-08-14 18:39:04 +0545

    Added login restriction

--- todoapp/views.py
+++ todoapp/views.py
@@ -3,30 +3,31 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Task
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
-class TaskList(ListView):
+class TaskList(LoginRequiredMixin,ListView):
     model = Task
     context_object_name = 'tasks'
     template_name = 'todoapp/tasklist.html'
 
-class TaskDetailView(DetailView):
+class TaskDetailView(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'tasks'
     template_name = 'todoapp/task_detail.html'
 
-class TaskCreateView(CreateView):
+class TaskCreateView(LoginRequiredMixin,CreateView):
     model = Task
     fields = '__all__'
     context_object_name = 'tasks'
     template_name = 'todoapp/task_create.html'
 
-class TaskUpdateView(UpdateView):
+class TaskUpdateView(LoginRequiredMixin,UpdateView):
     model = Task
     fields = '__all__'
     template_name='todoapp/task_update.html'
 
-class TaskDeleteView(DeleteView):
+class TaskDeleteView(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'tasks'
     template_name = 'todoapp/task_delete.html'
