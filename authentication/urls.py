3c96ba721d37e5fb8a7fb884b975ea9bf3352886
Ishan Shrestha <ishan.shrestha356@gmail.com>
2022-08-14 18:12:17 +0545

    added login and logout with SuccessMessageMixin

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import UserRegister
+from .views import UserRegister, UserLoginView
+from django.contrib.auth.views import LogoutView
 app_name = 'authentication'
 urlpatterns = [
     path('SignUp/', UserRegister.as_view(),name='signup'),
+    path('Login/',UserLoginView.as_view(),name='login'),
+    path('Logout', LogoutView.as_view(next_page='authentication:login'),name='logout'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,14 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import User
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.views import LoginView
+#from django.contrib.auth import logout
+from django.views import View
 # Create your views here.
 
-class UserRegister(CreateView):
+class UserRegister(SuccessMessageMixin,CreateView):
     form_class = SignUpForm
     #fields = ['first_name','last_name','username','email','password']
     template_name = 'authentication/sign_up.html'
     success_url = reverse_lazy('todoapp:tasklist')
+
+    def get_success_message(self,cleaned_data):
+        print(cleaned_data)
+        return "Account Successfully Created"
+
+class UserLoginView(SuccessMessageMixin,LoginView):
+    template_name = 'authentication/login.html'
+
+    def get_success_message(self,cleaned_data):
+        print(cleaned_data)
+        return "Logged In Success"
     
+# class UserLogoutView(SuccessMessageMixin,View):
+#     def get(self, request):
+#         logout(request)
+#         return redirect('todoapp:tasklist')
+    
+#     def get_success_message(self,cleaned_data):
+#         print(cleaned_data)
+#         return "Logout Success"
