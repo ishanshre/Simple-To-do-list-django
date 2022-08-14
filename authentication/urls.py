d04eb7b245e84605cf53bd7d2e4fa08f338895c1
Ishan Shrestha <ishan.shrestha356@gmail.com>
2022-08-14 17:26:52 +0545

    Added SignUpForm and signup function

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-
+from .views import UserRegister
 app_name = 'authentication'
 urlpatterns = [
-    
+    path('SignUp/', UserRegister.as_view(),name='signup'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import User
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 # Create your views here.
+
+class UserRegister(CreateView):
+    form_class = SignUpForm
+    #fields = ['first_name','last_name','username','email','password']
+    template_name = 'authentication/sign_up.html'
+    success_url = reverse_lazy('todoapp:tasklist')
+    
